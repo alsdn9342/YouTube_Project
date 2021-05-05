d7f524e5f984782d2b988949042587241a9b98e0
min woo <alsdn9342@gmail.com>
2021-05-06 01:47:36 +0900

    Fetch date from YouTube APIs

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,22 @@
 
+import { useEffect, useState } from 'react';
 import './app.css';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDSfx3ahDlVHxgqIqQVUgRJ3W1dPZQk08s", requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  },[videos]);
+
   return (
     <h1>Hello :)</h1>
   );
