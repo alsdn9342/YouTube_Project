2954803d6b611598504003322316fddf2cf7e220
min woo <alsdn9342@gmail.com>
2021-05-06 11:32:15 +0900

    make video item

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,4 @@
-
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
 import VideoList from './components/video_list/video_list';
 
@@ -9,18 +8,18 @@ function App() {
   useEffect(() => {
     const requestOptions = {
       method: 'GET',
-      redirect: 'follow'
+      redirect: 'follow',
     };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDSfx3ahDlVHxgqIqQVUgRJ3W1dPZQk08s", requestOptions)
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAtrSZkopq--QXlpEYQ5SrM9Kg5TZlZMl0',
+      requestOptions
+    )
       .then(response => response.json())
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
-  },[videos]);
-
-  return (
-    <VideoList videos={videos} />
-  );
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import VideoItem from '../video_item/video_item';
+import styles from './video_list.module.css';
 
-const VideoList = (props) => {
-    
-    return (
-    <ul>
-        {props.videos.map(video => (
-            <VideoItem key={video.id} video={video} />
-        ))}
+const VideoList = props => (
+    <ul className={styles.videos}>
+      {props.videos.map(video => (
+        <VideoItem key={video.id} video={video} />
+      ))}
     </ul>
-    );
-    
-};
+  );
+  
 
 export default VideoList;
