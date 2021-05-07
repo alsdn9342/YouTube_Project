d1c2586cba1948da3b9befc65e53feb1766fe429
min woo <alsdn9342@gmail.com>
2021-05-07 16:50:47 +0900

    Refactoring

--- src/app.jsx
+++ src/app.jsx
@@ -4,35 +4,19 @@ import Nav_bar from './components/Nav_bar/nav_bar';
 import VideoList from './components/video_list/video_list';
 import styles from './app.module.css';
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxresults=25&q=${query}&type=video&key=AIzaSyDSfx3ahDlVHxgqIqQVUgRJ3W1dPZQk08s`, requestOptions )
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id:item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
-  };
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos));
+  }
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDSfx3ahDlVHxgqIqQVUgRJ3W1dPZQk08s',
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube.mostPopular()
+    .then(videos => setVideos(videos));
   }, []);
+
   return(
     <div className="styles.app">
     <Nav_bar onSearch={search} />
